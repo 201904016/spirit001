2ef383498c234feaf2b2828266a52c9747177f3c
Junseo YOON <yunjunseo@naver.com>
2024-06-06 20:28:51 +0900

    MenuAddPage

--- src/pages/StoreInner/MenuaddPage.js
+++ src/pages/StoreInner/MenuaddPage.js
@@ -1,12 +1,158 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet, Dimensions} from 'react-native';
+import {ScrollView, TextInput} from 'react-native-gesture-handler';
+import CheckBox from '@react-native-community/checkbox';
 
 const MenuaddPage = () => {
   return (
-    <View>
-      <Text>메뉴 추가 페이지</Text>
+    <View style={styles.container}>
+      <ScrollView contentContainerStyle={styles.scrollContainer}>
+        <View style={styles.menuTopView}>
+          <View style={styles.menuHeader}>
+            <Text style={styles.menuHeaderText}>메뉴 1</Text>
+          </View>
+          <View style={styles.menuInputView}>
+            <Text style={styles.menuInputText}>메뉴 이름</Text>
+            <TextInput style={styles.menuInput} />
+          </View>
+          <View style={styles.menuInputView}>
+            <Text style={styles.menuInputText}>메뉴 가격</Text>
+            <TextInput style={styles.menuInput} />
+          </View>
+          <View style={styles.menuInputView}>
+            <Text style={styles.menuText}>메뉴 소개</Text>
+          </View>
+          <View style={styles.menuInputView}>
+            <TextInput style={styles.textarea} />
+          </View>
+          <View style={styles.checkBoxView}>
+            <CheckBox />
+            <Text style={styles.checkBoxText}>대표 메뉴 설정</Text>
+          </View>
+          <View style={styles.menuAddImage}>
+            <Text style={styles.menuText}>메뉴 사진 추가하기 (최대 1장)</Text>
+          </View>
+          <View style={styles.menuAddButton}>
+            <Text style={styles.menuAddText}>메뉴 추가</Text>
+          </View>
+          <View style={styles.saveButton}>
+            <Text style={styles.saveButtonText}>작성 완료</Text>
+          </View>
+        </View>
+      </ScrollView>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  scrollContainer: {
+    paddingBottom: 100, // 버튼 공간 확보
+  },
+  menuTopView: {
+    alignItems: 'center',
+  },
+  menuHeader: {
+    width: '80%',
+    marginTop: 20,
+    marginBottom: 10,
+  },
+  menuHeaderText: {
+    fontWeight: 'bold',
+    color: 'black',
+    fontSize: 15,
+    marginRight: 7,
+  },
+  menuInputView: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 10,
+    width: '80%',
+    marginVertical: 7,
+  },
+  menuInputText: {
+    fontWeight: '400',
+    color: 'black',
+    fontSize: 14,
+    marginRight: 7,
+  },
+  menuInput: {
+    backgroundColor: '#F2F2F2',
+    borderRadius: 10,
+    paddingVertical: 1,
+    flex: 1,
+  },
+  menuAddImage: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 20,
+    width: '80%',
+    marginVertical: 7,
+  },
+  menuText: {
+    fontWeight: '400',
+    color: 'black',
+    fontSize: 14,
+    marginVertical: 5,
+    marginBottom: 5,
+  },
+  menuArea: {
+    backgroundColor: '#F2F2F2',
+    width: '100%',
+    borderRadius: 10,
+    maxHeight: 200,
+    marginBottom: 10,
+  },
+  checkBoxView: {
+    width: '80%',
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    color: 'black',
+  },
+  checkBoxText: {
+    color: 'black',
+  },
+  saveButton: {
+    width: '80%',
+    marginBottom: 10,
+    marginTop: 30,
+    alignItems: 'center',
+    backgroundColor: '#16BBFF',
+    paddingVertical: 10,
+    borderRadius: 10,
+  },
+  saveButtonText: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 18,
+  },
+  menuAddButton: {
+    marginTop: 30,
+    color: 'white',
+    width: '20%',
+    height: 30,
+    fontSize: 12,
+    borderWidth: 1, // 외곽선 두께
+    borderColor: 'black', // 외곽선 색상
+    borderRadius: 10,
+    alignItems: 'center', // 수평 가운데 정렬
+    justifyContent: 'center', // 수직 가운데 정렬
+  },
+  menuAddText: {
+    color: 'black',
+    fontSize: 13,
+  },
+  textarea: {
+    backgroundColor: '#F2F2F2',
+    width: '100%',
+    borderRadius: 10,
+    maxHeight: 200,
+    marginBottom: 10,
+  },
+});
 
 export default MenuaddPage;

2f1fc8a808afd8358856644ebff4841e9e6827ce
Junseo YOON <yunjunseo@naver.com>
2024-07-09 00:54:41 +0900

    error fix

--- android/app/src/main/java/com/spirit001/MainApplication.java
+++ android/app/src/main/java/com/spirit001/MainApplication.java
@@ -4,7 +4,6 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import undefined.MapsPackage;
 import com.agontuk.RNFusedLocation.RNFusedLocationPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.swmansion.gesturehandler.RNGestureHandlerPackage;

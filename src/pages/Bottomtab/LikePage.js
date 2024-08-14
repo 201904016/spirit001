22a95d6f56d0a17f2aa7fc2f32f0b60967d067ec
Junseo YOON <yunjunseo@naver.com>
2024-08-15 01:38:18 +0900

    Liked

--- src/pages/Bottomtab/LikePage.js
+++ src/pages/Bottomtab/LikePage.js
@@ -1,17 +1,37 @@
 import React, {useEffect, useState} from 'react';
-import {View, Text, StyleSheet, Image, Pressable} from 'react-native';
-import {ScrollView} from 'react-native-gesture-handler';
+import {View, Text, FlatList, StyleSheet, Pressable, Image} from 'react-native';
 import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
-import Ionicons from 'react-native-vector-icons/Ionicons';
+import {getToken} from '../../store/Storage';
+import {ScrollView} from 'react-native-gesture-handler';
+import Categorytrans from '../../hooks/Categorytrans';
 
 const LikePage = () => {
+  const [token, setToken] = useState(null);
   const [stores, setStores] = useState([]);
+  const [categorys, setCategorys] = useState([]);
+
+  useEffect(() => {
+    const fetchToken = async () => {
+      const savedToken = await getToken();
+      console.log('저장된 토큰:', savedToken); // 저장된 토큰을 출력
+      setToken(savedToken);
+    };
+
+    fetchToken();
+  }, []);
+
   useEffect(() => {
     const getStoreData = () => {
+      if (!token) {
+        console.log('Token is not set');
+        return; // token이 없으면 더 이상 진행하지 않음
+      }
+
       fetch(`http://kymokim.iptime.org:11082/api/store/getLikedStore`, {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
+          'x-auth-token': token,
         },
       })
         .then(response => response.json())
@@ -24,106 +44,126 @@ const LikePage = () => {
     };
 
     getStoreData();
-  }, []);
+  }, [token]); // token이 변경될 때마다 이 useEffect가 실행됨
 
   return (
-    <ScrollView>
-      {stores.map(store => (
-        <Pressable
-          onPress={() =>
-            navigation.navigate('StoreMainPage', {storeId: store.storeId})
-          }>
-          <View style={styles.StoreTopView}>
-            <ScrollView
-              horizontal={true}
-              showsHorizontalScrollIndicator={false}>
-              <View style={styles.MenutopView}>
-                <Image
-                  source={require('../../assets/kim.png')} // menuItem에 이미지 URL이 있다고 가정
-                  style={styles.MenuImg}
-                />
-                <Text style={styles.MenuTitle}></Text>
-                <Text style={styles.MenuPrice}></Text>
-              </View>
-            </ScrollView>
-            <View style={styles.StoreView}>
-              <Text style={styles.StoreTitle}>{store.storeName}</Text>
-              <View style={styles.Storescore}>
-                <FontAwesomeIcon name="star" size={20} color="#FCC104" />
-                <Text style={styles.scoretext}>{store.storeRate}</Text>
-                <Text style={styles.scoretext}>
-                  고객리뷰 {store.reviewCount}
+    <View style={styles.container}>
+      <ScrollView>
+        {stores.map(store => (
+          <Pressable key={store.id}>
+            <View style={styles.storeInfo}>
+              <Image
+                source={require('../../assets/kim.png')}
+                style={styles.storeImage}
+              />
+              <View style={styles.text}>
+                <View style={styles.StoreView}>
+                  <Text
+                    style={styles.StoreTitle}
+                    numberOfLines={1}
+                    ellipsizeMode="tail">
+                    {store.storeName}
+                  </Text>
+                  <View style={styles.Storescore}>
+                    <FontAwesomeIcon name="star" size={15} color="#FCC104" />
+                    <Text style={styles.scoretext}>{store.storeRate}</Text>
+                  </View>
+                </View>
+                <View style={styles.StoreCategoryList}>
+                  <Text style={styles.StoreCategory}>
+                    {store.categories[0]}
+                  </Text>
+                  <Text style={styles.StoreCategory}>
+                    {store.categories[1]}
+                  </Text>
+                  <Text style={styles.StoreCategory}>
+                    {store.categories[2]}
+                  </Text>
+                </View>
+                <Text style={styles.StoreClose}>
+                  영업 종료 {store.closeHour}
                 </Text>
-                <Text style={styles.scoretext}>찜 {store.storeLikeCount}</Text>
-              </View>
-              <View style={styles.Storetime}>
-                <Ionicons name="time-outline" size={20} color="gray" />
-                <Text style={styles.scoretext}>
-                  영업중 {store.closeHour} 라스트 오더
-                </Text>
+                <Text style={styles.StoreMain}>대표 메뉴: </Text>
               </View>
             </View>
-            <View style={styles.line}></View>
-          </View>
-        </Pressable>
-      ))}
-    </ScrollView>
+          </Pressable>
+        ))}
+      </ScrollView>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
-  StoreTopView: {
-    marginVertical: 15,
-  },
-  MenutopView: {
-    marginHorizontal: 15,
-    alignItems: 'center',
-  },
-  MenuImg: {
-    width: 140,
-    resizeMode: 'contain',
-    height: 140,
-    borderRadius: 20,
-  },
-  MenuTitle: {
-    marginTop: 8,
-    fontWeight: 'bold',
-    fontSize: 16,
-  },
-  MenuPrice: {
-    marginTop: 3,
-    fontWeight: '700',
-    fontSize: 14,
+  container: {
+    width: '100%',
+    height: '100%',
+    backgroundColor: 'white',
   },
   StoreView: {
-    marginHorizontal: 15,
-    marginTop: 12,
-    alignItems: 'flex-start',
+    flexDirection: 'row',
+    justifyContent: 'space-between', // 이름과 별점 사이 공간 추가
+    alignItems: 'center',
+    width: 200, // 제한된 너비로 텍스트 줄임표 효과 강화
   },
+  StoreCategoryList: {
+    flexDirection: 'row',
+    marginRight: 5,
+  },
+
+  storeInfo: {
+    flexDirection: 'row',
+    backgroundColor: '#fff',
+    marginHorizontal: '5%',
+    padding: 15,
+    borderRadius: 12,
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: -2},
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    elevation: 10,
+    marginBottom: '5%',
+  },
+
+  storeImage: {
+    width: 150,
+    height: 150,
+    resizeMode: 'contain',
+    borderRadius: 10,
+  },
+  text: {
+    left: '7%',
+  },
+  scoretext: {
+    marginLeft: 3,
+  },
+
   StoreTitle: {
     fontWeight: 'bold',
     fontSize: 20,
     color: 'black',
+    maxWidth: 150, // 이름이 줄여지는 최대 너비 설정
+  },
+  StoreCategory: {
+    top: 10,
+    fontWeight: 'bold',
+    fontSize: 15,
+    color: 'black',
+    marginRight: 5,
+  },
+  StoreClose: {
+    top: 25,
+    fontSize: 15,
+    color: 'black',
+    marginRight: 5,
+  },
+  StoreMain: {
+    top: 62,
+    fontSize: 12,
+    color: 'black',
   },
   Storescore: {
     flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginTop: 4,
-  },
-  Storetime: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginTop: 4,
-  },
-  scoretext: {
-    marginLeft: 4,
-  },
-  line: {
-    marginTop: 15,
-    width: '100%',
-    height: 2,
-    borderColor: 'lightgray',
-    borderWidth: 1,
+    alignItems: 'center', // 별점과 텍스트를 정렬
   },
 });
 
